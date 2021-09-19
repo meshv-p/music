e601e7c0a507c78e54443ddf9c337955bd144fa2
Meshv Patel <77038149+meshv-p@users.noreply.github.com>
2021-09-19 22:32:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ function play(){
         document.getElementById('pause').style.display = "block";
         document.getElementById('play').style.display = "none";
        document.getElementById('track').play();
+       document.getElementById('start').innerHTML = Math.floor(track.currentTime/60);
+       document.getElementById('end').innerHTML = Math.floor(track.duration)/60;
+        document.getElementById('pro').Max = Math.floor(track.currentTime/60);
+
     }
     else{
         document.getElementById('pause').style.display = "none";
@@ -12,3 +16,27 @@ function play(){
         document.getElementById('track').pause();
     }
 }
+
+function formatTime(sec) {
+    let minutes = Math.floor(sec / 60);
+    let seconds = Math.floor(sec - minutes * 60);
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    return `${minutes}:${seconds}`;
+  }
+
+function progressValue() {
+  pro.max = track.duration;
+  pro.value = track.currentTime;
+
+  start.textContent = formatTime(track.currentTime);
+  end.textContent = formatTime(track.duration);
+}
+
+setInterval(progressValue, 500);
+function changeProgressBar() {
+    track.currentTime = pro.value;
+  }
+  
+pro.addEventListener("click", changeProgressBar);
