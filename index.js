b70a1690402791f7ea06a991e56ab399980091f0
Meshv Patel <77038149+meshv-p@users.noreply.github.com>
2021-09-20 15:48:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ console.log("hey");
 
 const song = ['Let Me Love You - DJ Snake  Justin Bieber (128kbps)-(MusicVilla.In).mp3',
                  'Live My Life (feat. Justin Bieber)-(DJmaza.in).mp3',
-                'Love_Me-Justin_Bieber[www.Mp3MaD.Com](1).mp3'];
+                'Love_Me-Justin_Bieber[www.Mp3MaD.Com](1).mp3',
+                "https://res.cloudinary.com/cbanlawi/video/upload/v1614140796/HarryStyles-WatermelonSugar_f5471p.mp3",
+                "https://res.cloudinary.com/cbanlawi/video/upload/v1614144520/Justin_Bieber-Yummy_vercib.mp3",
+              
+                "https://res.cloudinary.com/cbanlawi/video/upload/v1614186705/Loud_Luxury_ft._Brando_-_Body_fm7cdr.mp3"];
 
 document.getElementById('title').innerHTML = song[0];
 
@@ -57,11 +61,11 @@ function sec_less() {
     console.log(track.currentTime-10);
   }
   
-pro.addEventListener("input", changeProgressBar);
+pro.addEventListener("change", changeProgressBar);
 
 
 function nextsong(){
-  if(songindex <2){
+  if(songindex <song.length-1){
   songindex =songindex + 1;
   track.src = song[songindex];
   document.getElementById('title').innerHTML =song[songindex] ;
